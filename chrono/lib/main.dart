4fba54ccab9cee9ae72b7932534afb9a14ab21ca
Francesco Vianello <fra.vianello07@gmail.com>
2025-10-31 20:10:41 +0100

    logica finita

--- chrono/lib/main.dart
+++ chrono/lib/main.dart
@@ -1,12 +1,13 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 
+import 'package:flutter/material.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const ChronoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ChronoApp extends StatelessWidget {
+  const ChronoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,118 +15,91 @@ class MyApp extends StatelessWidget {
       title: 'Chrono',
       theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: const ChronoHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class ChronoHomePage extends StatefulWidget {
+  const ChronoHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => __MyHomePageState();
+  State<ChronoHomePage> createState() => _ChronoHomePageState();
 }
 
-class __MyHomePageState extends State<MyHomePage> {
-
+class _ChronoHomePageState extends State<ChronoHomePage> {
+  
   int _seconds = 0;
-  String _controlState = 'START'; 
-  String _pauseState = 'PAUSE'; 
-  bool _isRunning = false;
-  int _tuckCount = 0;
 
+  Stream<int>? _tickerStream;
   StreamSubscription<int>? _tickerSubscription;
+  Stream<int>? _secondsStream;
   StreamSubscription<int>? _secondsSubscription;
 
-  
+
   Stream<int> _createTickerStream() {
-    return Stream.periodic(
-      const Duration(milliseconds: 100),
-      (count) => count, 
-    );
+    return Stream.periodic(const Duration(milliseconds: 100), (count) => count);
   }
 
-  Stream<int> _createSecondsStream(Stream<int> tickerStream) {
-    return tickerStream.transform(
-      StreamTransformer<int, int>.fromHandlers(
-        handleData: (tuck, sink) {
-          _tuckCount++;
-        
-          if (mounted) {
-            setState(() {});
-          }
+  Stream<int> _createSecondsStream(Stream<int> ticker) async* {
+    int tuckCount = 0;
+    await for (final _ in ticker) {
+      tuckCount++;
+      if (tuckCount >= 10) {
+        tuckCount = 0;
+        yield 1;
+      }
+    }
+  }
 
-          if (_tuckCount >= 10) {
-            _tuckCount = 0; 
-            sink.add(1);
-          }
-        },
+
+    void _startStreams(){
+    
+      if(_tickerStream != null) return;
+      _tickerStream = _createTickerStream();
+      _secondsStream = _createSecondsStream(_tickerStream!);
+      _tickerSubscription = _tickerStream!.listen((_) {});
+      _secondsSubscription = _secondsStream!.listen((x) {
+        setState(() {
+          _seconds += x;
+        });
+      });
+    }
+
+    void _pauseStreams(){
+      
+      _tickerSubscription?.pause();
+      _secondsSubscription?.pause();
+    }
+
+    void _resumeStreams(){
+      
+      _tickerSubscription?.resume();
+      _secondsSubscription?.resume();
+    }
+
+    void _stopStreams(){
+      
+      _tickerSubscription?.cancel();
+      _secondsSubscription?.cancel();
+      _tickerSubscription = null;
+      _secondsSubscription = null;
+
+    }
+
+    void _formatTime(int totalSeconds) {
+      final mins = totalSeconds ~/ 60;
+      final secs = totalSeconds % 60;
+      
+}
+  //utilizzare streambuilder per aggiornare ui
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(s
+        child: Text('Chrono'),
+
       ),
     );
   }
-
-  void _startStreams() {
-    final tickerStream = _createTickerStream();
-    final secondsStream = _createSecondsStream(tickerStream);
-    _tickerSubscription = tickerStream.listen((_) {});
-    
-    _secondsSubscription = secondsStream.listen((secondIncrement) {
-      if (mounted) {
-        setState(() {
-          _seconds += secondIncrement;
-        });
-      }
-    });
-  }
-
-  void _stopStreams() {
-    _tickerSubscription?.cancel();
-    _secondsSubscription?.cancel();
-    _tickerSubscription = null;
-    _secondsSubscription = null;
-  }
-
-  void _handleControl() {
-    setState(() {
-      if (_controlState == 'START') {
-        _isRunning = true;
-        _controlState = 'STOP';
-        _pauseState = 'PAUSE';
-        _startStreams();
-      } else if (_controlState == 'STOP') {
-        _isRunning = false;
-        _controlState = 'RESET';
-        _stopStreams();
-      } else if (_controlState == 'RESET') {
-        _isRunning = false;
-        _seconds = 0;
-        _tuckCount = 0;
-        _controlState = 'START';
-        _pauseState = 'PAUSE';
-      }
-    });
-  }
-
-  
-  void _handlePause() {
-    if (!_isRunning) return;
-
-    setState(() {
-      if (_pauseState == 'PAUSE') {
-        _pauseState = 'RESUME';
-        _stopStreams();
-      } else {
-        _pauseState = 'PAUSE';
-        _startStreams();
-      }
-    });
-  }
-
-    String _formatTime(int totalSeconds) {
-    final mins = totalSeconds ~/ 60;
-    final secs = totalSeconds % 60;
-    return '${mins.toString().padLeft(2, '0')}:${secs.toString().padLeft(2, '0')}';
-  }
-
- 
 }
