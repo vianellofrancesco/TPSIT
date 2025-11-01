62d7483e81dd2d5a627de2f8bfdb877c85fddaa3
Francesco Vianello <fra.vianello07@gmail.com>
2025-11-01 19:58:27 +0100

    commit di consegna chrono

--- chrono/lib/main.dart
+++ chrono/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,78 +26,252 @@ class ChronoHomePage extends StatefulWidget {
   State<ChronoHomePage> createState() => _ChronoHomePageState();
 }
 
+enum ChronoState { start, stop, reset }
+enum PauseState { pause, resume }
+
 class _ChronoHomePageState extends State<ChronoHomePage> {
-  
   int _seconds = 0;
 
-  Stream<int>? _tickerStream;
-  StreamSubscription<int>? _tickerSubscription;
-  Stream<int>? _secondsStream;
-  StreamSubscription<int>? _secondsSubscription;
+  ChronoState _chronoState = ChronoState.start;
+  PauseState _pauseState = PauseState.pause;
 
+  StreamSubscription<int>? _subscription;
 
-  Stream<int> _createTickerStream() {
-    return Stream.periodic(const Duration(milliseconds: 100), (count) => count);
+  
+  Stream<int> _tickerStream() async* {
+    int tickCount = 0;
+    while (true) {
+      await Future.delayed(const Duration(milliseconds: 100));
+      yield tickCount++;
+    }
   }
 
-  Stream<int> _createSecondsStream(Stream<int> ticker) async* {
-    int tuckCount = 0;
-    await for (final _ in ticker) {
-      tuckCount++;
-      if (tuckCount >= 10) {
-        tuckCount = 0;
-        yield 1;
+  
+  Stream<int> _secondsStream(Stream<int> ticker) async* {
+    int currentSecond = 0;
+
+    await for (final tick in ticker) {
+      final newSecond = tick ~/ 10;
+
+      if (newSecond != currentSecond) {
+        currentSecond = newSecond;
+        yield newSecond;
       }
     }
   }
 
+  
+  void _startChrono() {
+    if (_subscription != null) return;
 
-    void _startStreams(){
-    
-      if(_tickerStream != null) return;
-      _tickerStream = _createTickerStream();
-      _secondsStream = _createSecondsStream(_tickerStream!);
-      _tickerSubscription = _tickerStream!.listen((_) {});
-      _secondsSubscription = _secondsStream!.listen((x) {
-        setState(() {
-          _seconds += x;
-        });
+    final ticker = _tickerStream();
+    final seconds = _secondsStream(ticker);
+
+    _subscription = seconds.listen((second) {
+      setState(() {
+        _seconds = second;
       });
+    });
+
+    setState(() {
+      _chronoState = ChronoState.stop;
+      _pauseState = PauseState.pause;
+    });
+  }
+
+  void _stopChrono() {
+    _subscription?.cancel();
+    _subscription = null;
+
+    setState(() {
+      _chronoState = ChronoState.reset;
+    });
+  }
+
+
+  void _resetChrono() {
+    setState(() {
+      _seconds = 0;
+      _chronoState = ChronoState.start;
+      _pauseState = PauseState.pause;
+    });
+  }
+
+  void _pauseChrono() {
+    _subscription?.pause();
+
+    setState(() {
+      _pauseState = PauseState.resume;
+    });
+  }
+
+  void _resumeChrono() {
+    _subscription?.resume();
+
+    setState(() {
+      _pauseState = PauseState.pause;
+    });
+  }
+
+ 
+  String _formatTime(int totalSeconds) {
+    final mins = totalSeconds ~/ 60;
+    final secs = totalSeconds % 60;
+    return '${mins.toString().padLeft(2, '0')}:${secs.toString().padLeft(2, '0')}';
+  }
+
+
+  void _handleChronoButton() {
+    switch (_chronoState) {
+      case ChronoState.start:
+        _startChrono();
+        break;
+      case ChronoState.stop:
+        _stopChrono();
+        break;
+      case ChronoState.reset:
+        _resetChrono();
+        break;
     }
+  }
 
-    void _pauseStreams(){
-      
-      _tickerSubscription?.pause();
-      _secondsSubscription?.pause();
+ 
+  void _handlePauseButton() {
+    if (_chronoState != ChronoState.stop) return;
+
+    switch (_pauseState) {
+      case PauseState.pause:
+        _pauseChrono();
+        break;
+      case PauseState.resume:
+        _resumeChrono();
+        break;
     }
+  }
 
-    void _resumeStreams(){
-      
-      _tickerSubscription?.resume();
-      _secondsSubscription?.resume();
-    }
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
 
-    void _stopStreams(){
-      
-      _tickerSubscription?.cancel();
-      _secondsSubscription?.cancel();
-      _tickerSubscription = null;
-      _secondsSubscription = null;
-
-    }
-
-    void _formatTime(int totalSeconds) {
-      final mins = totalSeconds ~/ 60;
-      final secs = totalSeconds % 60;
-      
-}
-  //utilizzare streambuilder per aggiornare ui
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(s
-        child: Text('Chrono'),
+    Color chronoColor;
+    IconData chronoIcon;
+    String chronoLabel;
 
+    switch (_chronoState) {
+      case ChronoState.start:
+        chronoColor = Colors.green;
+        chronoIcon = Icons.play_arrow;
+        chronoLabel = 'START';
+        break;
+      case ChronoState.stop:
+        chronoColor = Colors.red;
+        chronoIcon = Icons.stop;
+        chronoLabel = 'STOP';
+        break;
+      case ChronoState.reset:
+        chronoColor = Colors.blue;
+        chronoIcon = Icons.refresh;
+        chronoLabel = 'RESET';
+        break;
+    }
+
+    final isPauseEnabled = _chronoState == ChronoState.stop;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'CHRONO',
+          style: TextStyle(
+            letterSpacing: 4,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Center(
+        
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.1),
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+              color: Colors.white.withOpacity(0.1),
+              width: 2,
+            ),
+          ),
+          child: Text(
+            _formatTime(_seconds),
+            style: const TextStyle(
+              fontSize: 80,
+              fontWeight: FontWeight.w300,
+              fontFamily: 'monospace',
+              letterSpacing: 8,
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FloatingActionButton(
+                  heroTag: 'chronoBtn',
+                  onPressed: _handleChronoButton,
+                  backgroundColor: chronoColor,
+                  child: Icon(chronoIcon, size: 32),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  chronoLabel,
+                  style: const TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(width: 16),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FloatingActionButton(
+                  heroTag: 'pauseBtn',
+                  onPressed: isPauseEnabled ? _handlePauseButton : null,
+                  backgroundColor: isPauseEnabled
+                      ? (_pauseState == PauseState.pause
+                          ? Colors.orange
+                          : Colors.lightBlue)
+                      : Colors.grey,
+                  child: Icon(
+                    _pauseState == PauseState.pause
+                        ? Icons.pause
+                        : Icons.play_arrow,
+                    size: 32,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  _pauseState == PauseState.pause ? 'PAUSE' : 'RESUME',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                    color: isPauseEnabled ? Colors.white : Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
